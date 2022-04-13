756914014cbf48721f35f68d5335a9619880f193
dat dang <38239103+traxanhkho@users.noreply.github.com>
2022-04-13 19:40:43 +0700

    add pagination.css

--- animemely-app/src/components/common/Pagination.jsx
+++ animemely-app/src/components/common/Pagination.jsx
@@ -1,23 +1,38 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css" ; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../style/pagination.css";
 
 function Pagination(props) {
   return (
-    <nav aria-label="...">
-  <ul class="pagination">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active" aria-current="page">
-      <a class="page-link" href="#">2</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>
+    <nav >
+      <ul className="pagination">
+        <li className="page-item disabled">
+          <a className="page-link" href="#" aria-disabled="true">
+            Previous
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            1
+          </a>
+        </li>
+        <li className="page-item active" aria-current="page">
+          <a className="page-link" href="#">
+            2
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            3
+          </a>
+        </li>
+        <li className="page-item">
+          <a className="page-link" href="#">
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
